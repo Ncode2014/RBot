f8874e7640ba68de1072e3a8d6744de7516a68dc
Ncode2014 <private.nafi2016@gmail.com>
2021-03-29 23:50:28 +0700

    smd is useless since this bot has been takedown
    by indian cyber & some shit cyberpolice
    
    Signed-off-by: Ncode2014 <private.nafi2016@gmail.com>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -172,49 +172,6 @@ async def _(event):
         return
 
 
-@register(outgoing=True, pattern=r"^\.smd (?:(now)|(.*) - (.*))")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.pattern_match.group(1) == "now":
-        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
-        if playing is None:
-            return await event.edit("`Error: No scrobbling data found.`")
-        artist = playing.get_artist()
-        song = playing.get_title()
-    else:
-        artist = event.pattern_match.group(2)
-        song = event.pattern_match.group(3)
-    track = str(artist) + " - " + str(song)
-    chat = "@SpotifyMusicDownloaderBot"
-    try:
-        await event.edit("`Getting Your Music`")
-        async with bot.conversation(chat) as conv:
-            await asyncio.sleep(2)
-            await event.edit("`Downloading...`")
-            try:
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=752979930)
-                )
-                msg = await bot.send_message(chat, track)
-                respond = await response
-                res = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=752979930)
-                )
-                r = await res
-                await bot.send_read_acknowledge(conv.chat_id)
-            except YouBlockedUserError:
-                await event.reply("`Unblock `@SpotifyMusicDownloaderBot` and retry`")
-                return
-            await bot.forward_messages(event.chat_id, respond.message)
-        await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
-        await event.delete()
-    except TimeoutError:
-        return await event.edit(
-            "`Error: `@SpotifyMusicDownloaderBot` is not responding or Song not found!.`"
-        )
-
-
 @register(outgoing=True, pattern=r"^\.net (?:(now)|(.*) - (.*))")
 async def _(event):
     if event.fwd_from:
