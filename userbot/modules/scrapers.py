5fe3b0b6d71feac363cb5a8f644ef73783a1cd9d
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-05-26 21:07:11 +0700

    scrapers: nice things :)

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -541,7 +541,7 @@ async def yt_search(event):
             duration = i["duration"]
             views = i["views"]
             owner = "https://youtube.com" + i["owner_url"]
-            output += f"[{title}]({link})\nChannel: `{channel}`\nChannel Link: `{owner}`\nDuration: {duration} | {views}\n\n"
+            output += f"[{title}]({link})\nChannel: [{channel}]({owner})\nDuration: {duration} | {views}\n\n"
         except IndexError:
             break
 
